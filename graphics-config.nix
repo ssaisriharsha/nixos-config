f2af752ae3667c5d380661c2713b5d5954f8e194
Harsha <69705177+ssaisriharsha@users.noreply.github.com>
2025-09-06 21:42:38 +0530

    trying graphics configuration

--- graphics-config.nix
+++ graphics-config.nix
@@ -4,8 +4,8 @@
 {
   hardware.opengl = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true; # optional if you need 32-bit libs
+#   driSupport = true;
+#    driSupport32Bit = true; # optional if you need 32-bit libs
   };
   # If you need video acceleration (Intel), add extra packages:
   hardware.graphics.extraPackages = with pkgs; [ intel-media-driver ];
