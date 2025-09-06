630edac0e7659bbbaf4c6ee337542dcf1f48c304
Harsha <69705177+ssaisriharsha@users.noreply.github.com>
2025-09-06 23:29:42 +0530

    font installation

--- configuration.nix
+++ configuration.nix
@@ -151,5 +151,8 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
+	fonts.packages = with pkgs; [
+  	  nerd-fonts.jetbrains-mono
+	];
 
 }
